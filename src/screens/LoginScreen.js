321034717cebbd7999f78f0804476b5618f26d07
Beste Tokpınar <bstkpnr01@gmail.com>
2023-09-15 12:15:09 +0300

    usestate control mail and password

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -6,14 +6,27 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Email" />
-        <TextInput style={styles.input} placeholder="Şifre" />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={(e) => setEmail(e)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Şifre"
+          secureTextEntry
+          value={password}
+          onChange={(e) => setPassword(e)}
+        />
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
@@ -29,44 +42,42 @@ export default function LoginScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   inputContainer: {
-    width:'80%',
-    
+    width: "80%",
   },
   input: {
-    backgroundColor:'white',
-    paddingHorizontal:15,
-    paddingVertical:15,
-    marginTop:8,
-    borderRadius:8
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    marginTop: 8,
+    borderRadius: 8,
   },
   buttonContainer: {
-    width:'60%',
-    marginTop:40
+    width: "60%",
+    marginTop: 40,
   },
   button: {
-    padding:15,
-    backgroundColor:'#98EECC',
-    alignItems:'center',
-    borderRadius:8,
+    padding: 15,
+    backgroundColor: "#98EECC",
+    alignItems: "center",
+    borderRadius: 8,
   },
   label: {
-    color:'white',
-    fontSize:18,
-    fontWeight:'700'
+    color: "white",
+    fontSize: 18,
+    fontWeight: "700",
   },
   outlineButton: {
-    backgroundColor:'white',
-    marginTop:5
-
+    backgroundColor: "white",
+    marginTop: 5,
   },
   outlineLabel: {
-    color:'#98EECC',
-    fontSize:18,
-    fontWeight:'700'
+    color: "#98EECC",
+    fontSize: 18,
+    fontWeight: "700",
   },
 });
