7ae2d80f2fe973d02fdbfadc951f6f1643e919df
Beste Tokpınar <bstkpnr01@gmail.com>
2023-09-18 18:06:53 +0300

    edit fix

--- api/jsonServer.js
+++ api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://45cd-212-125-15-246.ngrok-free.app/",
+  baseURL: "https://2460-212-125-15-246.ngrok-free.app/",
 });
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,22 +5,55 @@ import {
   KeyboardAvoidingView,
   TextInput,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import React, { useState } from "react";
-import { auth } from "../../firebase";
-export default function LoginScreen() {
+import jsonServer from "../../api/jsonServer";
+
+export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSignUp = () => {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((userCredentials) => {
-        const user = userCredentials.user;
-        console.log("Kullanıcı", user.email);
-      })
-      .catch((error) => console.log(error.message));
+  const signIn = async () => {
+    try {
+      const response = await jsonServer.get("/users", {
+        params: { email, password },
+      });
+
+      if (response.data.length === 0) {
+        console.error("Hatalı e-posta adresi veya şifre.");
+        return;
+      }
+      navigation.navigate("Home");
+      console.log("Giriş başarılı:", response.data[0]);
+    } catch (error) {
+      console.error("Giriş sırasında hata", error.message);
+    }
   };
+
+  const signUp = async () => {
+    try {
+      const existingUserResponse = await jsonServer.get("/users", {
+        params: { email },
+      });
+
+      if (existingUserResponse.data.length > 0) {
+        console.error("Bu e-posta adresiyle zaten bir kullanıcı mevcut.");
+        return;
+      }
+      const response = await jsonServer.post("/users", {
+        email,
+        password,
+      });
+      signIn();
+
+      console.log("Kullanıcı başarıyla kaydedildi:", response.data);
+    } catch (error) {
+      console.error("Kayıt sırasında hata", error.message);
+    }
+  };
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
@@ -39,17 +72,21 @@ export default function LoginScreen() {
           onChange={(e) => setPassword(e)}
         />
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.label}>Giriş Yap</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.button, styles.outlineButton]}
-          onPress={handleSignUp}
-        >
-          <Text style={styles.outlineLabel}>Kayıt Ol</Text>
-        </TouchableOpacity>
-      </View>
+      {loading ? (
+        <ActivityIndicator size={"large"} color={"#0000ff"} />
+      ) : (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={signIn}>
+            <Text style={styles.label}>Giriş Yap</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.outlineButton]}
+            onPress={signUp}
+          >
+            <Text style={styles.outlineLabel}>Kayıt Ol</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </KeyboardAvoidingView>
   );
 }
